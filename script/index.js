7734754825e65667a053520f64998530237f682f
shemul345 <shemulchowdhury7684@gmail.com>
2025-09-13 16:10:15 +0600

    added full project with fully dynamic according to condition and ui fully responsive for mobile device

--- script/index.js
+++ script/index.js
@@ -8,6 +8,7 @@ const loadPlants = () => {
 };
 
 const loadCategoryPlants = (id) => {
+    manageSpinner(true);
     fetch(`https://openapi.programming-hero.com/api/category/${id}`)
         .then(res => res.json())
         .then(data => {
@@ -22,6 +23,16 @@ const removeActive = () => {
     const categoryBtn = document.querySelectorAll(".category-btn");
     categoryBtn.forEach(btn => btn.classList.remove("active"));
 };
+
+const manageSpinner = (status) => {
+    if (status == true) {
+        document.getElementById("spinner").classList.remove("hidden");
+        document.getElementById("plants-container").classList.add("hidden");
+    } else {
+        document.getElementById("plants-container").classList.remove("hidden");
+        document.getElementById("spinner").classList.add("hidden");
+    }
+}
 
 const loadCategory = () => {
     fetch("https://openapi.programming-hero.com/api/categories")
@@ -64,13 +75,13 @@ const displayCategory = (categories) => {
     categoryContainer.innerHTML = "";
 
     for (let category of categories) {
-        // console.log(category);
         const btnDiv = document.createElement("div");
         btnDiv.innerHTML = `
         <button id="category-btn-${category.id}"
         onclick="loadCategoryPlants(${category.id})"
-         class="text-left w-full px-2 py-2 hover:bg-green-700 hover:text-white rounded-lg category-btn">${category.category_name}</button>`;
-        // console.log(btnDiv);
+         class="text-left w-full px-2 py-2 hover:bg-green-700 hover:text-white rounded-lg category-btn">${category.category_name}</button>
+        `;
+        
         categoryContainer.append(btnDiv);
 
     };
@@ -81,7 +92,6 @@ const displayPlants = (plants) => {
     const plantsContainer = document.getElementById("plants-container");
     plantsContainer.innerHTML = "";
 
-     
     const limitWords = (text, limit) => {
         const words = text.split(" ");
         if (words.length > limit) {
@@ -94,7 +104,7 @@ const displayPlants = (plants) => {
       
         const plantDiv = document.createElement("div");
         plantDiv.innerHTML = `
-        <div class="card bg-base-100 w-82 shadow-sm">
+        <div class="card bg-base-100 w-60 md:w-82 shadow-sm">
                         <figure class="p-5">
                             <img onclick="loadPlantDetails(${plant.id})" class="rounded-xl h-52 w-full" src="${plant.image}" />
                         </figure>
@@ -115,6 +125,8 @@ const displayPlants = (plants) => {
         
         plantsContainer.append(plantDiv);
 
+        manageSpinner(false);
+
     };
 
 };
@@ -126,7 +138,6 @@ plantsContainer.addEventListener("click", (e) => {
          handleCart(e)
     };
 });
-
 
 const handleCart = (e) => {
     const plantName = e.target.parentNode.parentNode.children[0].innerText;
@@ -150,9 +161,6 @@ const handleCart = (e) => {
     showCarts(carts);
 };
 
-
-
-
 const showCarts = (carts) => {
     cartContainer.innerHTML = "";
     let total = 0;
@@ -162,7 +170,7 @@ const showCarts = (carts) => {
         cartContainer.innerHTML += `
         <div class="flex justify-between bg-green-100 p-3 rounded-xl mb-2">
             <div class="">
-                <h2 class="text-xl font-semibold ">${cart.title}</h2>
+                <h2 class="md:text-xl md:font-semibold ">${cart.title}</h2>
                 <p>$<span>${cart.price}</span> * <span>${cart.quantity}</span> = $<span>${itemTotal}</span></p>
             </div>
             <div class="">
@@ -181,8 +189,6 @@ const showCarts = (carts) => {
 
     document.querySelector(".flex.justify-between p span").innerText = total;
 };
-
-
 
 const cartDelete = (cartId) => {
     const filteredCarts = carts.filter(cart => cart.id !== cartId);
